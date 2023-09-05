88d26aadfbb267a42282d19bbbe4de371edee1a5
dmitriyfedotov <fedotovdmitriy14@gmail.com>
2023-09-06 00:50:54 +0300

    added actions to SubscriptionService

--- billing_service/billing_api/src/api/v1/actions.py
+++ billing_service/billing_api/src/api/v1/actions.py
@@ -1,16 +1,6 @@
-import json
-import logging
-import aiohttp as aiohttp
 from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.sql import text
-from confluent_kafka import Producer
 
-from src.db.base import get_session
-from src.modules.query import update_without_renew, get_renew_subscriptions
-from src.services.kafka import get_kafka
-from config import settings
-
+from src.services.subscriptions import SubscriptionService, get_subscriptions_service
 
 router = APIRouter()
 
@@ -22,24 +12,10 @@ router = APIRouter()
 )
 async def buy_subscription(
     user_id: str,
-    type_subscription_id: str
+    type_subscription_id: str,
+    subscription_service: SubscriptionService = Depends(get_subscriptions_service),
 ) -> str:
-    order_params = {'user_id': user_id,
-                    'type_subscribe_id': type_subscription_id}
-    order_url = f'http://{settings.billing.host}:{settings.billing.port}/api/v1/orders/'
-    async with aiohttp.ClientSession() as s:
-        async with s.post(url=order_url, params=order_params) as response:
-            if response.status == 200:
-                order_id = await response.json()
-
-    payment_params = {'order_id': order_id}
-    payment_url = f'http://{settings.billing.host}:{settings.billing.port}/api/v1/payments/'
-    async with aiohttp.ClientSession() as s:
-        async with s.post(url=payment_url, params=payment_params) as response:
-            if response.status == 200:
-                payment_link = await response.json()
-
-    return payment_link
+    return await subscription_service.buy_subscription(user_id, type_subscription_id)
 
 
 @router.put(
@@ -48,48 +24,6 @@ async def buy_subscription(
     summary='Disable expired subscription & renew',
 )
 async def change_subscription(
-    session: AsyncSession = Depends(get_session),
-    producer: Producer = Depends(get_kafka)
+    subscription_service: SubscriptionService = Depends(get_subscriptions_service),
 ) -> dict:
-
-    try:
-        query = await update_without_renew()
-        stmt = text(query)
-        res = await session.execute(stmt)
-        await session.commit()
-        data_subscribe = res.fetchall()
-        changed = [] if data_subscribe is None else [i[0] for i in data_subscribe]
-    except Exception as e:
-        logging.warning(f'Error: {str(e)}')
-
-    try:
-        query = await get_renew_subscriptions()
-        stmt = text(query)
-        res = await session.execute(stmt)
-        await session.commit()
-        data_subscribe = res.fetchall()
-
-        prolonging_subscriptions = [] if data_subscribe is None else \
-                                   [i._asdict() for i in data_subscribe]
-    except Exception as e:
-        logging.warning(f'Error: {str(e)}')
-
-    def acked(err, msg):
-        if err is not None:
-            logging.warning(f'Failed to deliver message: {str(msg)}: {str(err)}')
-        else:
-            logging.info(f'Message produced: {str(msg)}')
-
-    for i in prolonging_subscriptions:
-        dict_res = {
-            'user_id': str(i.get('user_id')),
-            'payment_id': str(i.get('payment_id')),
-            'price': str(i.get('price'))
-        }
-        producer.produce('prolong-topic', key=str(i.get('subscribe_id')), value=json.dumps(dict_res), callback=acked)
-        producer.poll(1)
-
-    return {
-        'changed': changed,
-        'prolonging': prolonging_subscriptions
-    }
+    return await subscription_service.change_subscription()
--- billing_service/billing_api/src/services/subscriptions.py
+++ billing_service/billing_api/src/services/subscriptions.py
@@ -2,17 +2,23 @@ import logging
 from datetime import datetime
 from decimal import Decimal
 
+import aiohttp
+from confluent_kafka import Producer
 from fastapi import HTTPException, Depends
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from config import settings
 from src.db.base import get_session
 from src.models.models import user_subscribes, type_subscribes
+from src.modules.query import update_without_renew, get_renew_subscriptions
+from src.services.kafka import get_kafka
 
 
 class SubscriptionService:
-    def __init__(self, session: AsyncSession) -> None:
+    def __init__(self, session: AsyncSession, producer: Producer) -> None:
         self.session = session
+        self.producer = producer
 
     async def get_subscriptions(self):
         try:
@@ -118,8 +124,73 @@ class SubscriptionService:
             logging.warning(f'Error: {str(e)}')
             raise HTTPException(status_code=404, detail="type not found")
 
+    async def buy_subscription(self, user_id: str, type_subscription_id: str) -> str:
+        order_params = {
+            'user_id': user_id,
+            'type_subscribe_id': type_subscription_id,
+        }
+        order_url = f'http://{settings.billing.host}:{settings.billing.port}/api/v1/orders/'
+        async with aiohttp.ClientSession() as s:
+            async with s.post(url=order_url, params=order_params) as response:
+                if response.status == 200:
+                    order_id = await response.json()
+
+        payment_params = {'order_id': order_id}
+        payment_url = f'http://{settings.billing.host}:{settings.billing.port}/api/v1/payments/'
+        async with aiohttp.ClientSession() as s:
+            async with s.post(url=payment_url, params=payment_params) as response:
+                if response.status == 200:
+                    payment_link = await response.json()
+
+        return payment_link
+
+    async def change_subscription(self) -> dict:
+
+        try:
+            query = await update_without_renew()
+            stmt = text(query)
+            res = await self.session.execute(stmt)
+            await self.session.commit()
+            data_subscribe = res.fetchall()
+            changed = [] if data_subscribe is None else [i[0] for i in data_subscribe]
+        except Exception as e:
+            logging.warning(f'Error: {str(e)}')
+
+        try:
+            query = await get_renew_subscriptions()
+            stmt = text(query)
+            res = await self.session.execute(stmt)
+            await self.session.commit()
+            data_subscribe = res.fetchall()
+
+            prolonging_subscriptions = [] if data_subscribe is None else [i._asdict() for i in data_subscribe]
+        except Exception as e:
+            logging.warning(f'Error: {str(e)}')
+
+        def acked(err, msg):
+            if err is not None:
+                logging.warning(f'Failed to deliver message: {str(msg)}: {str(err)}')
+            else:
+                logging.info(f'Message produced: {str(msg)}')
+
+        for i in prolonging_subscriptions:
+            dict_res = {
+                'user_id': str(i.get('user_id')),
+                'payment_id': str(i.get('payment_id')),
+                'price': str(i.get('price'))
+            }
+            self.producer.produce('prolong-topic', key=str(i.get('subscribe_id')), value=json.dumps(dict_res),
+                             callback=acked)
+            self.producer.poll(1)
+
+        return {
+            'changed': changed,
+            'prolonging': prolonging_subscriptions
+        }
+
 
 def get_subscriptions_service(
-        session: AsyncSession = Depends(get_session),
+    session: AsyncSession = Depends(get_session),
+    producer: Producer = Depends(get_kafka),
 ) -> SubscriptionService:
-    return SubscriptionService(session)
+    return SubscriptionService(session, producer)
